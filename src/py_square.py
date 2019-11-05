cb966dc6983f2e1e0b389929faa544650a093485
Prashanth Rajasekar <rprashanth3063@gmail.com>
2019-11-04 17:34:06 -0800

    comments have been added on the file while present on brnach-2. This is to test that the comments will not be visible on other branches.

--- src/py_square.py
+++ src/py_square.py
@@ -2,5 +2,7 @@
 def square(e):
 	# edited while pulling the code to the local machine.
 	return(e**2)
+# Added this comment when present on branch-1.
+# This comment should not be visible on other branches.
 
 print(square(5))
